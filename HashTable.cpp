60cb4492228d8ecdeb5384f6d59459e8ae763d77
Andrew Thomas <45573723+ElectricOneStar@users.noreply.github.com>
2020-05-30 18:10:39 -0700

    Add files via upload

--- HashTable.cpp
+++ HashTable.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <cmath>
 #include <iomanip>
+#include <fstream>
 using namespace std; // uses namespace std
 struct Student{ // creates the structure student
   char firstName[20]; // initializes variables
@@ -10,7 +11,7 @@ struct Student{ // creates the structure student
   int id;
   float gpa;
 };
-void Add(); // initializes the function
+void Add(Student** hashTable, char** firstName, char** lastName); // initializes the function
 void Subtract(vector<Student*>* v);
 void Print(vector<Student*>* v);
 int main() { // main function
@@ -20,6 +21,7 @@ int main() { // main function
   cout << "Welcome to Hash Table" << endl;
   char input[20];
   char quit[5];
+  bool invalid = false;
   //int ID;
   //int GPA;
   Student** hashTable = new Student*[100];
@@ -30,9 +32,48 @@ int main() { // main function
 	    cin.clear();
 	    cin.ignore();
 	    if(strcmp(input, "Add") == 0){ // ADD function
+	      char* firstNameFile = new char[20];
+	      char* secondNameFile = new char[20];
+	      cout << "Please enter the Firstname File" << endl;
+	      cin.get(firstNameFile, 20);
+	      cin.ignore();
+	      cin.clear();
+	      ifstream FirstNameFile;
+	      FirstNameFile.open(firstNameFile);
+	      if(!FirstNameFile){
+		cout << "invalid input" << endl;
+		invalid = true;
+		//return;
+	      }
+	      else{
+		//cout << "in" << endl;
+		
+		
+	      } 
+	      cout << "Please eneter the Lastname File" << endl;
+	        cin.get(secondNameFile, 20);
+	      cin.ignore();
+	      cin.clear();
+	       ifstream SecondNameFile;
+	      SecondNameFile.open(firstNameFile);
+	      if(!SecondNameFile){
+		cout << "invalid input" << endl;
+		invalid = true;
+	      }
+	      else{
+		//	cout << "in" << endl;
 		  } 
+	      if(invalid){
+		cout << "could not add" << endl;
+	      }
+	      else{
+		cout << "added" << endl;
+		}
+	      invalid = false;
+		} 
 	    else if(strcmp(input, "Delete") == 0){ // DELETE function
-	          }
+
+	    }
 	    else if(strcmp(input, "Quit") == 0){ // QUIT function
 	      stop = true; // sets the stop boolean to true
 	    }
@@ -49,7 +90,7 @@ int main() { // main function
     while(stop == false); // keeps going while stop is false
 	  return 0;
 }
-void Add(){ // creates the student pointer to add to vector
+void Add(Student** hashTable, char** firstName, char** lastName){ // creates the student pointer to add to vector
  }
 void Subtract(vector<Student*>* v){ // deletes the student from the vector
 
