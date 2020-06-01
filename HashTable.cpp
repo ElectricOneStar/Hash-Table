9da532882cd9773e60f4e95a432aae1279ff4638
Andrew Thomas <45573723+ElectricOneStar@users.noreply.github.com>
2020-06-01 11:21:29 -0700

    Add files via upload

--- HashTable.cpp
+++ HashTable.cpp
@@ -32,37 +32,70 @@ int main() { // main function
 	    cin.clear();
 	    cin.ignore();
 	    if(strcmp(input, "Add") == 0){ // ADD function
-	      char* firstNameFile = new char[20];
-	      char* secondNameFile = new char[20];
+	      int numberofnames;
+	      cout << "how many students would you like to add?" << endl;
+	      cin >> numberofnames;
+	      cin.clear();
+	      cin.ignore();
+	      char* firstNameFileName = new char[20];
+	      char* lastNameFileName = new char[20];
 	      cout << "Please enter the Firstname File" << endl;
-	      cin.get(firstNameFile, 20);
+	      cin.get(firstNameFileName, 20);
 	      cin.ignore();
 	      cin.clear();
-	      ifstream FirstNameFile;
-	      FirstNameFile.open(firstNameFile);
+	        char** FirstCollection = new char*[40];
+      for (int j = 0; j < 40; j ++) {
+        FirstCollection[j] = new char[20];
+      }
+      char First[20];
+ 	      ifstream FirstNameFile;
+	      FirstNameFile.open(firstNameFileName);
 	      if(!FirstNameFile){
 		cout << "invalid input" << endl;
 		invalid = true;
 		//return;
 	      }
 	      else{
+		int wordCounter = 0;
+	     while (wordCounter < 19) {
+          FirstNameFile.getline(First, 80, ',');
+	    strcpy(FirstCollection[wordCounter], First);
+          wordCounter++;
+        }
+		  
 		//cout << "in" << endl;
 		
 		
-	      } 
+	      }
+	      FirstNameFile.close();
+	      FirstNameFile.clear();
+      char** LastCollection = new char*[40];
+      for (int j = 0; j < 40; j ++) {
+        LastCollection[j] = new char[20];
+      }
+      char Last[20];
+
 	      cout << "Please eneter the Lastname File" << endl;
-	        cin.get(secondNameFile, 20);
+	        cin.get(lastNameFileName, 20);
 	      cin.ignore();
 	      cin.clear();
-	       ifstream SecondNameFile;
-	      SecondNameFile.open(firstNameFile);
-	      if(!SecondNameFile){
+	    ifstream LastNameFile;
+	     LastNameFile.open(lastNameFileName);
+	      if(!LastNameFile){
 		cout << "invalid input" << endl;
 		invalid = true;
 	      }
 	      else{
 		//	cout << "in" << endl;
-		  } 
+			int wordCounter2 = 0;
+	     while (wordCounter2 < 21) {
+          LastNameFile.getline(Last, 80, ',');
+	    strcpy(LastCollection[wordCounter2], Last);
+          wordCounter2++;
+        }
+	      }
+	       LastNameFile.close();
+	      LastNameFile.clear();
 	      if(invalid){
 		cout << "could not add" << endl;
 	      }
@@ -91,6 +124,7 @@ int main() { // main function
 	  return 0;
 }
 void Add(Student** hashTable, char** firstName, char** lastName){ // creates the student pointer to add to vector
+  
  }
 void Subtract(vector<Student*>* v){ // deletes the student from the vector
 
